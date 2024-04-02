59888ba823d906c3798112303cdb55ef17544b21
Jaeltaka <takasjaels@yahoo.com>
2024-04-02 10:15:04 +0200

    connexion api front

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { Form, FormGroup, FormBuilder,FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import{UserServices} from '../service/user.service'
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../interface/user';
+
 
 @Component({
   selector: 'app-inscription',
@@ -10,27 +15,42 @@ import { Router } from '@angular/router';
 
 export class InscriptionComponent {
   public connexionForm!: FormGroup
-  constructor(private formBuilder: FormBuilder , private router: Router) { }
+  constructor(private formBuilder: FormBuilder , private router: Router,public user:UserServices,private http: HttpClient) { }
   hide = true;
-  email = new FormControl('', [Validators.required, Validators.email]);
+  
+  
   ngOnInit(): void {
     this.connexionForm = this.formBuilder.group({
-      password: [""],
-      nom:[""],
-      prenom:[""]
+      
+      nom:new FormControl('',[Validators.required]),
+      prenom:new FormControl('',[Validators.required]),
+      mdp:new FormControl('',[Validators.required]),
+      mail : new FormControl('', [Validators.required, Validators.email]),
+      
       
     })
+    
    
   }
   navigateToConnexion() {
     this.router.navigate(['/connexion']);
+    console.log(this.connexionForm.value,'sa foctionne');
   }
   getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
+    if (this.formBuilder.control('required')) {
+      return 'entrez les valeurs';
     }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    return this.formBuilder.control('mail') ? 'Not a valid email' : '';
   }
+  postUsers() {
+    return this.http.post('http://localhost:3000/api/user',this.connexionForm.value).subscribe(
+    (reponse)=>{
+      this.navigateToConnexion()
+    },
+    (error)=>{}
+    );
+  }
+  
 }
 
