6011f1a233ed7d8a6a2103b9a1ab24640d7cd420
Jaeltaka <takasjaels@yahoo.com>
2024-05-10 19:00:07 +0200

    ajout du fichier environnement,changement du call api

--- src/app/service/booking.ts
+++ src/app/service/booking.ts
@@ -2,15 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { book } from '../model/booking'; ;
-
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookinService {
     constructor(private http:HttpClient){}
+    private BACK_URL = environment.apiURL
     getBooking(): Observable<book[]>{
      const userId=localStorage.getItem('id')
-        return this.http.get<book[]>("http://localhost:3000/api/booking/getAllBook/"+userId)
+        return this.http.get<book[]>(this.BACK_URL+"/booking/getAllBook/"+userId)
     }
 }
