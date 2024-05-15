90bde594fbcaaa58754ba64d81d833a1d3f352b7
Pascal190 <115409615+Pascal190@users.noreply.github.com>
2024-05-15 15:51:29 +0200

    requete

--- src/app/service/booking.ts
+++ src/app/service/booking.ts
@@ -9,8 +9,18 @@ import { book } from '../model/booking'; ;
 })
 export class BookinService {
     constructor(private http:HttpClient){}
+    private BACK_URL = 'http://localhost:3000/api/booking';
+
     getBooking(): Observable<book[]>{
      const userId=localStorage.getItem('id')
-        return this.http.get<book[]>("http://localhost:3000/api/booking/getAllBook/"+userId)
+        return this.http.get<book[]>(this.BACK_URL+'/getAllBook/'+userId)
     }
+
+      // tu doit recup id de la destinition choisi par l'user
+    postBooking(booking: any){
+      
+      return this.http.post<book[]>(this.BACK_URL+'/myBook', booking)
+    }
+
+
 }
